427111ae2d1694908315a01a0064ee664dcde405
Random-Nam3 <115816841+Random-Nam3@users.noreply.github.com>
2025-02-06 12:27:22 +0000

    Update pointCloudGen.py
    Removed Print statement
    Overwrite setting for output file

--- pointCloudGen.py
+++ pointCloudGen.py
@@ -7,9 +7,8 @@ def colour_to_depth(colour):
 #Assumed Image taken from directly above, and the coolour relates to the depth
 def get_point_cloud(inputImage, outputFileName):
     im = Image.open(pathlib.Path(inputImage))
-    outputFile = open(outputFileName)
+    outputFile = open(outputFileName,"w")
     pix = im.load()
-    print(im.size)
     for x in range(im.size[0]):
         for y in range(im.size[1]):
             outputFile.write(str(x)+','+str(y)+','+str(colour_to_depth(pix[x,y]))+'\n')
