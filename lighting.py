da3ef0dfa3a1cb608231eeda6cb16bc012b1c3fa
WillChit <149966154+WillChit@users.noreply.github.com>
2025-02-18 12:53:04 +0000

    Update lighting.py
    uses numpy instead to be faster

--- lighting.py
+++ lighting.py
@@ -1,6 +1,8 @@
 from krita import *
 import struct
 import math
+import numpy as np
+import time
 
 def dot(u, v): #return the dot product of  two vectors
     if len(u) != len(v):
@@ -11,42 +13,52 @@ def dot(u, v): #return the dot product of  two vectors
 def get_shadow_node(normal_map, direction, doc): 
     #arguments: array of vectors, direction to the light source, document
     #all vector arguments should be normalised
+    #normal_map and direction should both be numpy arrays
     
     result = doc.createNode("shadow", "paintLayer")
     width = len(normal_map[0])
     height = len(normal_map)
+
+    #get light intensity for each pixel
+    dots = direction * normal_map
+    dots = np.sum(dots, axis=2)
+    dots[dots < 0] = 0
+
+    #convert light intensity to alpha value in rbga
+    gamma = np.ones([height, width], dtype=int) * 255 - ((dots ** (1/2.2)) * 255).astype(np.int32)
+    gamma = np.clip(gamma, 0, 255)
     
-    pixel_data = bytearray()
-    for y in range(height):
-        for x in range(width):
-            intensity = dot(normal_map[y][x], direction)
-            intensity = max(intensity, 0)
-            gamma = 255 - int(255 * (intensity ** (1/2.2)))
-            
-            gamma = min(gamma, 255)
-            gamma = max(gamma, 0)
-            colour = (0, 0, 0, gamma)
-            pixel_data.extend(struct.pack('BBBB', *colour))
+    #convert to krita expected format
+    colours = np.zeros([height, width, 3])
+    gamma = np.expand_dims(gamma, axis=-1)
+    colours = np.concatenate((colours, gamma), axis=-1)
+
+    #convert to byte array
+    colours = colours.astype(np.ubyte)
+    pixel_data = colours.tobytes()
             
     result.setPixelData(pixel_data, 0, 0, width, height)
     return result
     
     
 def gen_funny_normal_map(width, height):
-    result = [[(0, 0, 0) for y in range(width)] for x in range(height)]
-    for j in range(height):
-        for i in range(width):
-            scale_factor = 1/50
-            x = i * scale_factor
-            y = j * scale_factor
-            result[j][i] = (-math.cos(x) * scale_factor, math.sin(y) * scale_factor, 1)
-            magnitude = result[j][i][0] * result[j][i][0]
-            magnitude += result[j][i][1] * result[j][i][1]
-            magnitude += result[j][i][2] * result[j][i][2]
-            magnitude= math.sqrt(magnitude)
-            if (magnitude == 0):
-                continue
-            result[j][i] = (result[j][i][0] / magnitude, result[j][i][1] / magnitude, result[j][i][2] / magnitude)
+    scale_factor = 1/50
+    x = np.array([-math.cos(i * scale_factor) * scale_factor for i in range(width)])
+    y = np.array([math.sin(i * scale_factor) * scale_factor for i in range(height)])
+
+    x = np.expand_dims(x, axis=0)
+    x = np.repeat(x, height, axis=0)
+    #print(x.shape)
+    y = np.expand_dims(y, axis=1)
+    y = np.repeat(y, width, axis=1)
+    z = np.ones([height, width, 1])
+
+    x = np.expand_dims(x, axis=-1)
+    y = np.expand_dims(y, axis=-1)
+
+    result = np.concatenate((x, y, z), axis=-1)
+    magnitude = np.linalg.norm(result, axis=-1, keepdims=True)
+    result = result / magnitude
     return result
      
     
@@ -62,7 +74,11 @@ height = doc.height()
 
 normal_map = gen_funny_normal_map(width, height)
 
-shadow_node = get_shadow_node(normal_map, (0.95 * 3.5, 0.2207 * 3.5, 0.2207 * 3.5), doc)
+start_time = time.time()
+shadow_node = get_shadow_node(normal_map, np.array([2, 1, 0.3]), doc)
+end_time = time.time()
+
+print(end_time - start_time)
 
 doc.rootNode().addChildNode(shadow_node, None)
 doc.refreshProjection()
