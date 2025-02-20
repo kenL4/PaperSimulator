607f0e0c837dba54f2e773d2778c6758a928216b
kenL4 <74073301+kenL4@users.noreply.github.com>
2025-02-20 11:15:51 +0000

    Added apply texture

---
+++ krita_plugin/paper/modules/apply_texture.py
@@ -0,0 +1,49 @@
+from krita import *
+import shutil
+import os
+
+def make_texture(texture_path, pattern_name):
+    path = Krita.instance().readSetting("", "ResourceDirectory", None)
+    path = os.path.join(path, "patterns")
+    pattern_file_path = path + "/" + pattern_name
+    
+    # Just overwrite, for now
+    try:
+        shutil.copyfile(texture_path, pattern_file_path)
+    except:
+        print("Could not write to path!")
+    print(pattern_file_path)
+    return pattern_file_path
+
+def apply_texture(texture_path):
+    PATTERN_NAME = "papertest.jpg"
+    
+    # Ensure pattern exists
+    assets_folder = os.path.join(os.path.dirname(__file__), "../assets")
+    path = make_texture(os.path.join(assets_folder, texture_path), PATTERN_NAME)
+    
+    # Apply paper file as pattern
+    patterns_manager = Krita.instance().resources("pattern")
+    pattern = None
+    for name, res in patterns_manager.items():
+        if name == PATTERN_NAME:
+            print("Pattern file found!")
+            pattern = res
+    
+    view = Krita.instance().activeWindow().activeView()
+    if view and pattern:
+        # Enable patterns
+        preset = Preset(view.currentBrushPreset())
+        print(preset)
+        
+        # Overwrite pattern data
+        pattern = view.currentPattern()
+        pattern.setImage(QImage(path))
+        view.activateResource(pattern)
+        
+        doc = Krita.instance().activeDocument()
+        doc.refreshProjection()
+    else:
+        print("Failed to apply pattern!")
+
+apply_texture("papertest.jpg")
