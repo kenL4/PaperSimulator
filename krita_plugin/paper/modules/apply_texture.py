8cb1e78ab35648c883d27ff6bc1ea9ab681bdd22
kenL4 <74073301+kenL4@users.noreply.github.com>
2025-02-24 14:49:47 +0000

    Added pattern param :fire:

--- krita_plugin/paper/modules/apply_texture.py
+++ krita_plugin/paper/modules/apply_texture.py
@@ -1,6 +1,7 @@
 from krita import *
 import shutil
 import os
+import xml.etree.ElementTree as ET 
 
 def make_texture(texture_path, pattern_name):
     path = Krita.instance().readSetting("", "ResourceDirectory", None)
@@ -8,16 +9,14 @@ def make_texture(texture_path, pattern_name):
     pattern_file_path = path + "/" + pattern_name
     
     # Just overwrite, for now
-    try:
-        shutil.copyfile(texture_path, pattern_file_path)
-    except:
-        print("Could not write to path!")
+    shutil.copyfile(texture_path, pattern_file_path)
     print(pattern_file_path)
     return pattern_file_path
 
 def apply_texture(texture_path, PATTERN_NAME):
     # Ensure pattern exists
     path = make_texture(texture_path, PATTERN_NAME)
+    # Krita.instance().resourcesChanged()
     
     # Apply paper file as pattern
     patterns_manager = Krita.instance().resources("pattern")
@@ -30,14 +29,31 @@ def apply_texture(texture_path, PATTERN_NAME):
     view = Krita.instance().activeWindow().activeView()
     if view and pattern:
         # Enable patterns
-        # preset = Preset(view.currentBrushPreset())
-        # print(preset)
+        preset = Preset(view.currentBrushPreset())
+        root = ET.fromstring(preset.toXML())
+        
+        written = False
+        for param in root.findall("param"):
+            if param is not None and param.attrib["name"] == "Texture/Pattern/Enabled":
+                written = True
+                param.text = "![CDATA[true]]"
+                
+        if not written:
+            pattern_param = ET.Element("param", type="string", name="Texture/Pattern/Enabled")
+            pattern_param.text = "![CDATA[true]]"
+            root.append(pattern_param)
+        
+        xml_string = ET.tostring(root, encoding="utf-8").decode("utf-8")
+        # print(xml_string)
+        view.setCurrentBrushPreset(preset.fromXML(xml_string))
         
         # Set the current pattern
+        view.activateResource(pattern)
         view.setCurrentPattern(pattern)
-        view.activateResource("pattern")
         
         doc = Krita.instance().activeDocument()
         doc.refreshProjection()
     else:
-        print("Failed to apply pattern!")
+        print("Failed to find pattern!")
+
+apply_texture("/home/ken/Documents/Part1B/Lent/GroupProject/PaperSimulator/krita_plugin/paper/assets/papertest.jpg", "papertest.jpg")
